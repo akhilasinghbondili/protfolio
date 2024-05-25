0f3438dedff8c6d02032f8983394673d9725bb3c
akhilasinghbondili <bondiliakhilasingh@gmail.com>
2024-05-25 11:30:31 +0530

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+// var typed = new Typed(".text", {
+//     // Strings: ["front-end developer", "Trainee engineer"],
+//     typespeed: 100,
+//     backspeed: 100,
+//     backdelay: 1000,
+//     loop: true
+// })
+var typed = new Typed('#element', {
+    strings: ['Trainee Engineer','Front-End developer'],
+    typeSpeed: 50,
+    loop:true
+  });
